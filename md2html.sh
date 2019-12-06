6aa5c0fe0fad9c9950909ab26783e27567b9aa38
vbalasubramaniam <vbalasubramaniam@trifacta.com>
2019-12-06 08:53:08 -0800

    Added md2html

---
+++ md2html.sh
@@ -0,0 +1,10 @@
+echo "Convert markdown to html"
+echo "USAGE: ./md2html index"
+echo "The above command reads index.md and produces index.html"
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+  else
+    pandoc -s -o $1.html $1.md
+    echo "$1.html is ready!"
+fi
